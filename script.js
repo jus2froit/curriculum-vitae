eaff30e749cd41df4cbdd310b5891b634e646dbd
jus2froit <juliandevienne@orange.fr>
2022-07-27 12:02:28 +0200

    add passions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var skills = ["CSS", "HTML", "JS", "..."]
+var skills = ["CSS", "HTML", "JS", "Tailwind CSS", "symfony", "vuejs", "..."]
+
+var passions = ["Foot","Sports", "Manga/Anime", "Jeux vidéos"]
 
 for (i = 0; i < skills.length; i++){
     document.getElementById("mySkills").innerHTML += 
@@ -8,3 +10,12 @@ for (i = 0; i < skills.length; i++){
                                 </li>
                                 `
 }
+
+document.getElementById("passionsButton").addEventListener("click", function(){
+    for (i = 0; i < passions.length; i++){
+        document.getElementById("myPassions").innerHTML += 
+        `
+            <li class="flex justify-center  p-2"> ${passions[i]}</li>
+        `
+    }
+})
