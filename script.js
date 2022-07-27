4fe6a2af60130b2f48ddd466f2c776f10de5cd5b
jus2froit <juliandevienne@orange.fr>
2022-07-27 13:54:55 +0200

    fix button click

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ var skills = ["CSS", "HTML", "JS", "Tailwind CSS", "symfony", "vuejs", "..."]
 
 var passions = ["Foot","Sports", "Manga/Anime", "Jeux vidéos"]
 
+var nbClick = 0
+
 for (i = 0; i < skills.length; i++){
     document.getElementById("mySkills").innerHTML += 
                                 `
-                                <li class="flex justify-center  text-white">
+                                <li class="flex justify-center  text-fuchsia-800">
                                     ${skills[i]}
                                 </li>
                                 `
 }
 
 document.getElementById("passionsButton").addEventListener("click", function(){
-    for (i = 0; i < passions.length; i++){
-        document.getElementById("myPassions").innerHTML += 
-        `
-            <li class="flex justify-center  p-2"> ${passions[i]}</li>
-        `
+    nbClick += 1
+    if (nbClick <= 1){
+        for (i = 0; i < passions.length; i++){
+            document.getElementById("myPassions").innerHTML += 
+            `
+                <li class="flex justify-center  p-2"> ${passions[i]}</li>
+            `
+        }
     }
 })
